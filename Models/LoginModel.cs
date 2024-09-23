092e24646698a8ed4ecda7d92fe5d138c866ca21
Myutopiauniverse <s224040405@mandela.ac.za>
2024-09-23 14:07:24 +0200

    Initial commit message

---
+++ Models/LoginModel.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HelloBlazorServer.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Username is required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters long")]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
